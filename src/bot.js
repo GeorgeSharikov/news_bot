80d523509723f97f6d262c0842ae4948f1877a6e
George <g-sharikov@bk.ru>
2021-11-03 22:47:07 +0300

    added express server

--- src/bot.js
+++ src/bot.js
@@ -5,14 +5,21 @@ import {NewsController} from "./Controllers/newsController.js";
 import {getMainMenu} from "./keyboard/keyboards.js";
 import {sequalize} from "../db/db.js";
 import {NewsModel} from "../db/models.js";
+import express from 'express'
 
+//server
+const port = process.env.PORT || 3000
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world!!!'))
+app.listen(port, () => console.log('listen'))
+// db
 await sequalize.authenticate()
 await sequalize.sync()
-
+//bot
 const API = process.env.API_KEY
 
 const bot = new Telegraf(API)
 bot.start((ctx) => ctx.reply('Welcome!!! \nHere you can get some news in easy finnish', getMainMenu()))
 bot.hears('Get News', async (ctx) => await NewsController.getTodayNews(ctx))
-
 bot.launch()
