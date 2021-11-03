dbe27aea7cffa1a2185e2182c2e916dfabb7ca7a
George <g-sharikov@bk.ru>
2021-11-03 23:27:06 +0300

    added ping feature to heroku

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ import {getMainMenu} from "./keyboard/keyboards.js";
 import {sequalize} from "../db/db.js";
 import {NewsModel} from "../db/models.js";
 import express from 'express'
+import fetch from 'node-fetch';
 
 //server
 const port = process.env.PORT || 3000
@@ -23,3 +24,8 @@ const bot = new Telegraf(API)
 bot.start((ctx) => ctx.reply('Welcome!!! \nHere you can get some news in easy finnish', getMainMenu()))
 bot.hears('Get News', async (ctx) => await NewsController.getTodayNews(ctx))
 bot.launch()
+
+
+setInterval(async () => {
+    fetch('https://finnish-news-bot.herokuapp.com/')
+}, 60*30)
