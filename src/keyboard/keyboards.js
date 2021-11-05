942095382d4cdb9a0444405d98574e0ec8fa2e5e
George <g-sharikov@bk.ru>
2021-11-05 21:25:05 +0300

    fix view bug

--- src/keyboard/keyboards.js
+++ src/keyboard/keyboards.js
@@ -9,6 +9,6 @@ export const getMainMenu = () => {
 export const getPreviousNewsInlineMenu = (news) => {
     return Markup.inlineKeyboard(news.map(el => {
         const date = el.date.toString()
-        return Markup.button.callback(date, date)
+        return [Markup.button.callback(date, date)]
     }))
 }
